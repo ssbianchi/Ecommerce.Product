cff63114d3239bc6afdce9622a08d9d11caa236c
Sergio Bianchi <ss.bianchi@gmail.com>
2024-03-02 16:41:11 -0300

    Update UnitOfWork Repository

--- Ecommerce.Product.Repository/Context/UnitOfWork.cs
+++ Ecommerce.Product.Repository/Context/UnitOfWork.cs
@@ -1,11 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Ecommerce.Product.CrossCutting.Database;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ecommerce.Product.Repository.Context
 {
